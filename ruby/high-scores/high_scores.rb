ec29dd574e16a88facc7cfb5bbe7f1de0fae784c
Abhinav Keswani <abhinav.keswani@gmail.com>
2023-02-27 15:01:13 +1300

    Ruby. High scores. Complete.

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,23 @@
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores[-1]
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.sort.reverse.take(3)
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
